8e5c7a418033a16c80f452ee80bec5368402113f
Phainix <28295747+Phainix@users.noreply.github.com>
2019-09-22 16:37:21 +0100

    Verifying Test

--- app/src/androidTest/java/com/example/faitha/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/faitha/notekeeper/NoteCreationTest.java
@@ -15,6 +15,9 @@ import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static org.hamcrest.Matchers.*;
 import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+
+import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
@@ -39,11 +42,27 @@ public class NoteCreationTest {
 
           onView(withId(R.id.spinner_courses)).perform(click());
           onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+          onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(
+                  containsString(course.getTitle())
+          )));
 
-          onView(withId(R.id.text_note_title)).perform(typeText(noteTitle));
-          onView(withId(R.id.text_note_body)).perform(typeText(noteText),
+
+          onView(withId(R.id.text_note_title)).perform(typeText(noteTitle))
+                  .check(matches(withText(containsString(noteTitle))));
+
+          onView(withId(R.id.text_note_text)).perform(typeText(noteText),
                   closeSoftKeyboard());
 
+          onView(withId(R.id.text_note_text)).check(matches(withText(containsString(noteText))));
+
           pressBack();
+
+          int noteIndex = sDataManager.getNotes().size() - 1;
+          NoteInfo note = sDataManager.getNotes().get(noteIndex);
+
+          assertEquals(course, note.getCourse());
+          assertEquals(noteTitle, note.getTitle());
+          assertEquals(noteText, note.getText());
+
      }
 }
